028ddfd0f8ca2042e0e7407879b4f4a43046283a
Pavel Klimenkov <ps.klimenkov@gmail.com>
2017-05-04 17:21:58 +0300

    added speller

---
+++ lib/speller.rb
@@ -0,0 +1,38 @@
+# Class to spell digits through letters associated
+class Speller
+  attr_reader :config, :data
+  DATA_DIRECTORY = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'data')
+
+  def initialize
+    @config = Config.new
+    load
+  end
+
+  def data_directory
+    DATA_DIRECTORY
+  end
+
+  def convert(numbers)
+    regex = transform(numbers)
+    @data.select { |word| word =~ /#{regex}/ }
+  end
+
+  private
+
+  def load
+    @data = []
+    Dir[File.join(data_directory, '**', '*.txt')].each do |fp|
+      @data += File.open(fp).read.split("\n")
+    end
+    @data.uniq!
+  end
+
+  def transform(numbers)
+    regex = ''
+    numbers.to_s.split('').each do |digit|
+      chars = Speller.new.config[:digits][digit.to_i].map { |char| "#{char.upcase}#{char.downcase}" }.join('')
+      regex << "[#{chars}]{1}"
+    end
+    "^#{regex}$"
+  end
+end
